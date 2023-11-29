ab7c2391ccff857d2a46a83dcc9d5a6f66633d36
Yann-Laurick Abé <44323843+yannlaurickabe@users.noreply.github.com>
2023-11-29 18:17:06 +0000

    Complete Exercise 1 Part 1

--- src/main/java/org/advent/day1/Elf.java
+++ src/main/java/org/advent/day1/Elf.java
@@ -15,10 +15,16 @@ public class Elf implements Comparable<Elf> {
         return totalCalories;
     }
 
-    @Override
-    public int compareTo(Elf arg0) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    public void addCalories(int caloriesToAdd) {
+        totalCalories += caloriesToAdd;
     }
 
+    @Override
+    public int compareTo(Elf other) {
+        if (totalCalories > other.getTotalCalories()) 
+            return 1;
+        else if (totalCalories < other.getTotalCalories())
+            return -1;
+        else return 0;
+    }
 }
--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -1,7 +1,5 @@
 package org.advent.day1;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -16,21 +14,37 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addCalories(1000);
+        assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        elf1.addCalories(1000);
+
+        Elf elf2 = new Elf();
+        elf2.addCalories(2000);
+
+        assertThat(elf2.compareTo(elf1), equalTo(1));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        Elf elf2 = new Elf();
+        assertThat(elf2.compareTo(elf1), equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        elf1.addCalories(1000);
+
+        Elf elf2 = new Elf();
+        elf2.addCalories(2000);
+
+        assertThat(elf1.compareTo(elf2), equalTo(-1));
     }
 }
